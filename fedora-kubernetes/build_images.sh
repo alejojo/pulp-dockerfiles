371bda084e8b5e93ef272b3bed389442ac24a5b2
Mark Lamourine <mlamouri@redhat.com>
2014-08-22 13:42:52 +0000

    added qpid, mongo and content-volumes to build

--- fedora-kubernetes/build_images.sh
+++ fedora-kubernetes/build_images.sh
@@ -4,8 +4,11 @@
 #
 REGISTRY=${1:-docker-registry.usersys.redhat.com}
 DEVUSER=${2:-markllama}
+docker build -t ${REGISTRY}/${DEVUSER}/mongodb images/mongodb
+docker build -t ${REGISTRY}/${DEVUSER}/qpid images/qpid
 docker build -t ${REGISTRY}/${DEVUSER}/pulp-base images/pulp-base
 docker build -t ${REGISTRY}/${DEVUSER}/pulp-beat images/pulp-beat
 docker build -t ${REGISTRY}/${DEVUSER}/pulp-resource-manager images/pulp-resource-manager
+docker build -t ${REGISTRY}/${DEVUSER}/pulp-content-volumes images/pulp-content-volumes
 docker build -t ${REGISTRY}/${DEVUSER}/pulp-worker images/pulp-worker
 docker build -t ${REGISTRY}/${DEVUSER}/pulp-apache images/pulp-apache
